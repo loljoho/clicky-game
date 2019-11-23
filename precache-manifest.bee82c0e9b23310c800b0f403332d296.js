27d98ac331a01e862bbe7c39c0b61741bd40cde9
loljoho <loljoho@users.noreply.github.com>
2019-11-23 11:37:57 -0500

    Updates

---
+++ precache-manifest.bee82c0e9b23310c800b0f403332d296.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c49791cab33254a55601ad07ade9e60",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "9162a0f6bc5e60531583",
+    "url": "/clicky-game/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d608997eae406e8b28ae",
+    "url": "/clicky-game/static/js/2.4f56e48f.chunk.js"
+  },
+  {
+    "revision": "9162a0f6bc5e60531583",
+    "url": "/clicky-game/static/js/main.b057f521.chunk.js"
+  },
+  {
+    "revision": "aee7aa09d3bc81ff757d",
+    "url": "/clicky-game/static/js/runtime~main.0b3343a4.js"
+  }
+]);
